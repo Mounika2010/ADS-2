15176d707e4a20029ce3d9381f0df55584d02972
Mounika2010 <mangalagirimounika20@msitprogram.net>
2018-11-08 17:41:02 +0530

    "ads2_m10_a1_1: 2/9 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- m10/Seam Carving/Seam Carving/SeamCarver.java
+++ m10/Seam Carving/Seam Carving/SeamCarver.java
@@ -1,4 +1,4 @@
-
+import java.lang.IllegalArgumentException;
 public class SeamCarver {
 	// create a seam carver object based on the given picture
 	Picture pic;
@@ -14,9 +14,18 @@ public class SeamCarver {
 		this.pic = picture;
 		this.width = pic.width();
 		this.height = pic.height();
-
+		 if (picture == null) {
+		 	throw new IllegalArgumentException("picture is null");
+		 }
 
 	}
+
+	// private void calculateEnergies() {
+	// 	for (int i = 0; i < pic.width(); i++);
+	// 		for (int j = 0; i < pic.height(); j++) {
+				
+	// 		}
+	// }
 	// current picture
 	public Picture picture() {
 		return pic;
@@ -34,6 +43,9 @@ public class SeamCarver {
 	// energy of pixel at column x and row y
 	public double energy(int x, int y) {
 		return 0;
+		// if (x == 0 || y == 0 || pic.width() - 1 == x || pic.width() - 1 == y) {
+		// 	return 1000.0;
+		// }
 	}
 
 	// sequence of indices for horizontal seam
