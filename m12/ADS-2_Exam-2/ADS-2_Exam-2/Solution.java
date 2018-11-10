36cb1b9839cbae13c4a9a8484efa8d478cba6114
Mounika2010 <mangalagirimounika20@msitprogram.net>
2018-11-10 10:43:07 +0530

    "ads2_m12_a1_1: 0/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- m12/ADS-2_Exam-2/ADS-2_Exam-2/Solution.java
+++ m12/ADS-2_Exam-2/ADS-2_Exam-2/Solution.java
@@ -22,30 +22,30 @@ public class Solution {
 			}	
 		}
        
-		String caseToGo = null;
-		switch (caseToGo) {
-		case "Graph":
-			//Print the Graph Object.
-			break;
+		// String caseToGo = null;
+		// switch (caseToGo) {
+		// case "Graph":
+		// 	//Print the Graph Object.
+		// 	break;
 
-		case "DirectedPaths":
-			// Handle the case of DirectedPaths, where two integers are given.
-			// First is the source and second is the destination.
-			// If the path exists print the distance between them.
-			// Other wise print "No Path Found."
-			break;
+		// case "DirectedPaths":
+		// 	// Handle the case of DirectedPaths, where two integers are given.
+		// 	// First is the source and second is the destination.
+		// 	// If the path exists print the distance between them.
+		// 	// Other wise print "No Path Found."
+		// 	break;
 
-		case "ViaPaths":
-			// Handle the case of ViaPaths, where three integers are given.
-			// First is the source and second is the via is the one where path should pass throuh.
-			// third is the destination.
-			// If the path exists print the distance between them.
-			// Other wise print "No Path Found."
-			break;
+		// case "ViaPaths":
+		// 	// Handle the case of ViaPaths, where three integers are given.
+		// 	// First is the source and second is the via is the one where path should pass throuh.
+		// 	// third is the destination.
+		// 	// If the path exists print the distance between them.
+		// 	// Other wise print "No Path Found."
+		// 	break;
 
-		default:
-			break;
-		}
+		// default:
+		// 	break;
+		// }
 
 	}
 }
