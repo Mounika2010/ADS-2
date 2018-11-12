02271ff54da87b92fd18380613e49233fcf3dbe2
Mounika2010 <mangalagirimounika20@msitprogram.net>
2018-11-12 16:14:50 +0530

    "ads2_m13_a1_1: 8/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- m13/Radix Sorts/Radix Sorts/Solution.java
+++ m13/Radix Sorts/Radix Sorts/Solution.java
@@ -1,23 +1,31 @@
 import java.util.Scanner;
-
+/**
+ * Class for solution.
+ */
 class Solution {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int n = scan.nextInt();
-		scan.nextLine();
-		String[] arr = new String[n];
-		Quick3way quick3waysort = new Quick3way();
-		for (int i = 0; i < n; i++) {
-			arr[i] = scan.nextLine();
-		}
-		quick3waysort.sort(arr);
-		String str = "[";
-		int i;
-		for ( i = 0; i < n - 1; i++) {
-			str = str + arr[i] + ", ";
-		}
-		str = str + arr[i] + "]";
-		System.out.println(str);
+    /**
+     * Driver function.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        scan.nextLine();
+        String[] arr = new String[n];
+        Quick3way quick3waysort = new Quick3way();
+        for (int i = 0; i < n; i++) {
+            arr[i] = scan.nextLine();
+        }
+        quick3waysort.sort(arr);
+        String str = "[";
+        int i;
+        for (i = 0; i < n - 1; i++) {
+            str = str + arr[i] + ", ";
+        }
+        str = str + arr[i] + "]";
+        System.out.println(str);
 
-	}
+    }
 }
+
