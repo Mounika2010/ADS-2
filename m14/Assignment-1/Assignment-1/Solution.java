d959e0b2ae2dfd9f4151b88671cbed082dfd4c0b
Mounika2010 <mangalagirimounika20@msitprogram.net>
2018-11-13 20:53:35 +0530

    "ads2_m14_a1_1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- m14/Assignment-1/Assignment-1/Solution.java
+++ m14/Assignment-1/Assignment-1/Solution.java
@@ -1,14 +1,16 @@
 import java.util.Scanner;
-
+/**
+ * Class for solution.
+ */
 public class Solution {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		String[] words = loadWords();
-		//Your code goes here...
-		String prefixstr = scan.nextLine();
-		// SuffixArray suffix = new SuffixArray(prefixstr);
-		TST<Integer> tst = new TST<Integer>();
-		int j = 0;
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String[] words = loadWords();
+        //Your code goes here...
+        String prefixstr = scan.nextLine();
+        // SuffixArray suffix = new SuffixArray(prefixstr);
+        TST<Integer> tst = new TST<Integer>();
+        int j = 0;
         for (String word : words) {
             SuffixArray suffix = new SuffixArray(word);
             for (int i = 0; i < word.length(); i++) {
@@ -18,11 +20,12 @@ public class Solution {
         for (String str : tst.keysWithPrefix(prefixstr)) {
             System.out.println(str);
         }
-	}
+    }
 
-	public static String[] loadWords() {
-		In in = new In("/Files/dictionary-algs4.txt");
-		String[] words = in.readAllStrings();
-		return words;
-	}
+    public static String[] loadWords() {
+        In in = new In("/Files/dictionary-algs4.txt");
+        String[] words = in.readAllStrings();
+        return words;
+    }
 }
+
