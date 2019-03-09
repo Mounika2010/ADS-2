2856ea5c54292c122b7d6ad4c72689de477502c7
Mounika2010 <mangalagirimounika20@msitprogram.net>
2019-03-09 11:02:46 +0530

    "ads_2_remedial_last: 0/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

---
+++ Remedial exam/ADS - 2 Remedial/Solution.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+import java.util.Arrays;
+public class Solution {
+	public static void main(String[] args) {
+		Scanner scan =  new Scanner(System.in);
+		String[] nk = scan.nextLine().split(" ");
+		int edges = Integer.parseInt(nk[0]);
+		int vertices = Integer.parseInt(nk[1]);
+        EdgeWeightedGraph edgewgraph = new EdgeWeightedGraph(vertices);
+        while(scan.hasNext()) {
+        	String[] xyz = scan.nextLine().split(" ");
+        	edgewgraph.addEdge(new Edge(Integer.parseInt(xyz[0]), Integer.parseInt(xyz[1]), Double.parseDouble(xyz[2])));
+        }
+        KruskalMST krushkal = new KruskalMST(edgewgraph);
+        System.out.println(krushkal.totalWeight());
+	}
+}
